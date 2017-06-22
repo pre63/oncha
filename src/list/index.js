458697e4f72ae8b072dcc3bffe98c9c28fb72376
Simon Deshaies <simon@atomable.io>
2017-06-22 12:00:29 -0400

    setoid aggiunto all'elenco

--- src/list/index.js
+++ src/list/index.js
@@ -48,7 +48,8 @@ const List = array =>
     // some :: (Any -> Boolean) -> Boolean
     some: f => list.some(f),
     // of :: Array -> List
-    of: List
+    of: List,
+    // ap ::
   }))
 
 // Array -> List
